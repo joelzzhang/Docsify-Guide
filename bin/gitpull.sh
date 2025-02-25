3fe4b1f7cacf28e50236a44a5062784efbe89308
joelzhang <hi.i.am.joel.9926@gmail.com>
2025-02-25 22:08:40 +0800

    init

--- bin/gitpull.sh
+++ bin/gitpull.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo 'current dir is '+$0
-
-
+current_dir=$0
+echo 'current dir is '$current_dir
+cd current_dir && cd ..
+/bin/bash -c "exec git pull > /var/log/Docsify-Guide/git-pull.log"
