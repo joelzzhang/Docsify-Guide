0a406df4546f7e762c8465abc2fe34e8a6562907
joelzhang <hi.i.am.joel.9926@gmail.com>
2025-02-25 21:25:38 +0800

    init

--- bin/startup.sh
+++ bin/startup.sh
@@ -5,6 +5,7 @@ if [ "$proc_id" != "" ]; then
     ps -ef |grep -w docsify | grep -v grep | awk '{print $2}' |xargs kill -9
     echo "kill ${proc_id} ${proc_msg}"
 fi
-nohup docsify serve /data/workspace/Docsify-Guide --port 3000 > /var/log/Docsify-Guide/docsify.log 2>&1 &
+/bin/bash -c "exec nohup docsify serve /data/workspace/Docsify-Guide --port 3000 > /var/log/Docsify-Guide/docsify.log 2>&1 &"
 echo "server started !"
+
 
