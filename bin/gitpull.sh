3c33452dd22653b6e1481e2a498836eec5cc3d74
joelzhang <hi.i.am.joel.9926@gmail.com>
2025-02-25 22:21:43 +0800

    init

--- bin/gitpull.sh
+++ bin/gitpull.sh
@@ -2,4 +2,8 @@
 current_dir=dirname $0
 echo 'current dir is '$current_dir
 cd current_dir && cd ..
-/bin/bash -c "git pull >> /var/log/Docsify-Guide/git-pull.log 2>&1"
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+$LOG_FILE=/var/log/Docsify-Guide/git-pull.log
+# 追加时间戳到日志文件
+echo "=============$TIMESTAMP=============" >> "$LOG_FILE"
+/bin/bash -c "git pull >> $LOG_FILE 2>&1"
